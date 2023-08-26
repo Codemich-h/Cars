315ab2fed4c40e722103b1c6dc1b9f46d7cfde3a
Codemich-h <michebastos61@gmail.com>
2023-08-26 18:32:26 +0530

    Full App

---
+++ car/src/App.js
@@ -0,0 +1,16 @@
+import CarForm from './Components/CarForm';
+import CarList from './Components/CarList';
+import CarSearch from './Components/CarSearch';
+import CarValue from './Components/CarValue';
+function App() {
+  return (
+    <div className="App">
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
+}
+
+export default App;
---
+++ car/src/Components/CarList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CarList() {
+  return (
+    <div>CarList</div>
+  )
+}
+
+export default CarList
+
